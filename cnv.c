930dc50796bab7f0b3b807de62aefe35045b1e87
Adam Starak <starak.adam@gmail.com>
2016-06-01 13:57:04 +0200

    prototype of cnv.c

---
+++ cnv.c
@@ -0,0 +1,129 @@
+
+/*-
+ * Copyright (c) 2016 Adam Starak <starak.adam@gmail.com>
+ * All rights reserved.
+ *
+ * This software was developed by Pawel Jakub Dawidek under sponsorship from
+ * the FreeBSD Foundation.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#ifdef _KERNEL
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+#include <sys/malloc.h>
+
+#include <machine/stdarg.h>
+
+#else
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#endif
+
+#include <sys/dnv.h>
+#include <sys/nv.h>
+
+#include "cnv.h"
+#include "nv_impl.h"
+
+#define	CNVLIST_GET(ftype, type, nvtype)				\
+ftype									\
+cnvlist_get_##type(void **cookiep)					\
+{									\
+									\
+	if (nvpair_type(*cookiep) == NV_TYPE_##nvtype)			\
+		return (nvpair_get_##type(*cookiep));			\
+	return (NULL);							\
+}
+
+CNVLIST_GET(bool, bool, BOOL)
+CNVLIST_GET(uint64_t, number, NUMBER)
+CNVLIST_GET(const char *, string, STRING)
+CNVLIST_GET(const nvlist_t *, nvlist, NVLIST)
+CNVLIST_GET(int, descriptor, DESCRIPTOR)
+
+#undef	DNVLIST_GET
+
+const void *
+cnvlist_get_binary(void **cookiep, size_t *sizep)
+{
+	const void *value;
+	if (nvpair_type(*cookiep) == NV_TYPE_BINARY) {
+		value = nvpair_get_binary(*cookiep, sizep);
+		return (value);
+	}
+	return (NULL);
+}
+
+#define	CNVLIST_TAKE(ftype, type, nvtype)			    	\
+ftype									\
+dnvlist_take_##type(nvlist_t *nvl, void **cookiep)			\
+{									\
+	ftype value = cnvlist_get_##type(cookiep);			\
+	nvlist_remove_nvpair(nvl, *cookiep);				\
+	return (value);							\
+}
+
+CNVLIST_TAKE(bool, bool, BOOL)
+CNVLIST_TAKE(uint64_t, number, NUMBER)
+CNVLIST_TAKE(const char *, string, STRING)
+CNVLIST_TAKE(const nvlist_t *, nvlist, NVLIST)
+CNVLIST_TAKE(int, descriptor, DESCRIPTOR)
+
+#undef	CNVLIST_TAKE
+
+void *
+cnvlist_take_binary(nvlist_t *nvl, void **cookiep, size_t *sizep)
+{
+	const void *value = cnvlist_get_binary(cookiep, sizep);
+	nvlist_remove_nvpair(nvl, *cookiep);
+	return (value);
+}
+
+#define	CNVLIST_FREE(ftype, type, nvtype)			    	\
+void									\
+dnvlist_FREE_##type(nvlist_t *nvl, void **cookiep)			\
+{									\
+	nvlist_remove_nvpair(nvl, *cookiep);				\
+}
+
+CNVLIST_FREE(bool, bool, BOOL)
+CNVLIST_FREE(uint64_t, number, NUMBER)
+CNVLIST_FREE(const char *, string, STRING)
+CNVLIST_FREE(const nvlist_t *, nvlist, NVLIST)
+CNVLIST_FREE(int, descriptor, DESCRIPTOR)
+
+#undef	CNVLIST_TAKE
+
+
+
--- cnv.h
+++ cnv.h
@@ -1,14 +1,45 @@
-/*
- * TODO: add legal note
- * TODO: what if cookiep is bad?
+/*-
+ * Copyright (c) 2016 Adam Starak <starak.adam@gmail.com>
+ * All rights reserved.
+ *
+ * This software was developed by Pawel Jakub Dawidek under sponsorship from
+ * the FreeBSD Foundation.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
  */
 
 #ifndef _CNV_H_
 #define _CNV_H_
 
-//
-// TODO: add includes 
-//
+#include <sys/cdefs.h>
+
+#ifndef	_NVPAIR_T_DECLARED
+#define	_NVPAIR_T_DECLARED
+struct nvpair;
+
+typedef struct nvpair nvpair_t;
+#endif
 
 #ifndef _NVLIST_T_DECLARED
 #define _NVLIST_T_DECLARED
@@ -20,67 +51,17 @@ typedef struct nvlist nvlist_t;
 __BEGIN_DECLS
 
 /*
- * The nvlist_add functions add the given name/value pair and return the cookie for it.
- * If a pointer is provided, nvlist_add will internally allocate memory for the
- * given data (in other words it won't consume provided buffer).
- */
-
-void *cnvlist_add_null(nvlist_t *nvl, const char *name);
-void *cnvlist_add_bool(nvlist_t *nvl, const char *name, bool value);
-void *cnvlist_add_number(nvlist_t *nvl, const char *name, uint64_t value);
-void *cnvlist_add_string(nvlist_t *nvl, const char *name, const char *value);
-void *cnvlist_add_stringf(nvlist_t *nvl, const char *name, const char *valuefmt, ...) __printflike(3, 4);
-#if !defined(_KERNEL) || defined(_VA_LIST_DECLARED)
-void *cnvlist_add_stringv(nvlist_t *nvl, const char *name, const char *valuefmt, va_list valueap) __printflike(3, 0);
-#endif
-void *cnvlist_add_nvlist(nvlist_t *nvl, const char *name, const nvlist_t *value);
-void *cnvlist_add_binary(nvlist_t *nvl, const char *name, const void *value, size_t size);
-void *cnvlist_add_bool_array(nvlist_t *nvl, const char *name, const bool *value, size_t nitems);
-void *cnvlist_add_number_array(nvlist_t *nvl, const char *name, const uint64_t *value, size_t nitems);
-void *cnvlist_add_string_array(nvlist_t *nvl, const char *name, const char * const *value, size_t nitems);
-void *cnvlist_add_nvlist_array(nvlist_t *nvl, const char *name, const nvlist_t * const *value, size_t nitems);
-#ifndef _KERNEL
-void *cnvlist_add_descriptor(nvlist_t *nvl, const char *name, int value);
-void *cnvlist_add_descriptor_array(nvlist_t *nvl, const char *name, const int *value, size_t nitems);
-#endif
-
-/*
- * The cnvlist_move functions add the given name/value pair and returns cookie associated with it.
- * The functions consumes provided buffer.
- */
-
-void *cnvlist_move_string(nvlist_t *nvl, const char *name, char *value);
-void *cnvlist_move_nvlist(nvlist_t *nvl, const char *name, nvlist_t *value);
-void *cnvlist_move_binary(nvlist_t *nvl, const char *name, void *value, size_t size);
-void *cnvlist_move_bool_array(nvlist_t *nvl, const char *name, bool *value, size_t nitems);
-void *cnvlist_move_string_array(nvlist_t *nvl, const char *name, char **value, size_t nitems);
-void *cnvlist_move_nvlist_array(nvlist_t *nvl, const char *name, nvlist_t **value, size_t nitems);
-void *cnvlist_move_number_array(nvlist_t *nvl, const char *name, uint64_t *value, size_t nitems);
-#ifndef _KERNEL
-void *cnvlist_move_descriptor(nvlist_t *nvl, const char *name, int value);
-void *cnvlist_move_descriptor_array(nvlist_t *nvl, const char *name, int *value, size_t nitems);
-#endif
-
-/*
  * The cnvlist_get functions returns value associated with the given cookie.
  * If it returns a pointer, the pointer represents internal buffer and should
  * not be freed by the caller.
- * TODO: justify?
  */
 
-bool			 cnvlist_get_bool(const nvlist_t *nvl, void **cookiep);
-uint64_t		 cnvlist_get_number(const nvlist_t *nvl, void **cookiep);
-void **cookiep		*cnvlist_get_string(const nvlist_t *nvl, void **cookiep);
-const nvlist_t		*cnvlist_get_nvlist(const nvlist_t *nvl, void **cookiep);
-const void		*cnvlist_get_binary(const nvlist_t *nvl, void **cookiep, size_t *sizep);
-const bool		*cnvlist_get_bool_array(const nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-const uint64_t		*cnvlist_get_number_array(const nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-void **cookiep * const	*cnvlist_get_string_array(const nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-const nvlist_t * const	*cnvlist_get_nvlist_array(const nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-#ifndef _KERNEL
-int			 cnvlist_get_descriptor(const nvlist_t *nvl, void **cookiep);
-const int		*cnvlist_get_descriptor_array(const nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-#endif
+bool			cnvlist_get_bool(void **cookiep);
+uint64_t		cnvlist_get_number(void **cookiep);
+const char  	       *cnvlist_get_string(void **cookiep);
+const nvlist_t	       *cnvlist_get_nvlist(void **cookiep);
+const void	       *cnvlist_get_binary(void **cookiep, size_t *sizep);
+int			cnvlist_get_descriptor(void **cookiep);
 
 /*
  * The cnvlist_take functions returns value associated with the given cookie and
@@ -88,43 +69,24 @@ const int		*cnvlist_get_descriptor_array(const nvlist_t *nvl, void **cookiep, si
  * The caller is responsible for freeing received data.
  */
 
-bool		  cnvlist_take_bool(nvlist_t *nvl, void **cookiep);
-uint64_t	  cnvlist_take_number(nvlist_t *nvl, void **cookiep);
-char		 *cnvlist_take_string(nvlist_t *nvl, void **cookiep);
-nvlist_t	 *cnvlist_take_nvlist(nvlist_t *nvl, void **cookiep);
-void		 *cnvlist_take_binary(nvlist_t *nvl, void **cookiep, size_t *sizep);
-bool		 *cnvlist_take_bool_array(nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-uint64_t	 *cnvlist_take_number_array(nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-char		**cnvlist_take_string_array(nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-nvlist_t	**cnvlist_take_nvlist_array(nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-#ifndef _KERNEL
-int		 cnvlist_take_descriptor(nvlist_t *nvl, void **cookiep);
-int		 *cnvlist_take_descriptor_array(nvlist_t *nvl, void **cookiep, size_t *nitemsp);
-#endif
+bool	    cnvlist_take_bool(nvlist_t *nvl, void **cookiep);
+uint64_t    cnvlist_take_number(nvlist_t *nvl, void **cookiep);
+char	   *cnvlist_take_string(nvlist_t *nvl, void **cookiep);
+nvlist_t   *cnvlist_take_nvlist(nvlist_t *nvl, void **cookiep);
+void	   *cnvlist_take_binary(nvlist_t *nvl, void **cookiep, size_t *sizep);
+int	    cnvlist_take_descriptor(nvlist_t *nvl, void **cookiep);
 
 /*
  * The cnvlist_free functions removes the given name/value pair from the nvlist based on cookie
  * and frees memory associated with it.
  */
 
-void cnvlist_free(nvlist_t *nvl, void **cookiep);
-void cnvlist_free_type(nvlist_t *nvl, void **cookiep, int type);
-
-void cnvlist_free_null(nvlist_t *nvl, void **cookiep);
 void cnvlist_free_bool(nvlist_t *nvl, void **cookiep);
 void cnvlist_free_number(nvlist_t *nvl, void **cookiep);
 void cnvlist_free_string(nvlist_t *nvl, void **cookiep);
 void cnvlist_free_nvlist(nvlist_t *nvl, void **cookiep);
 void cnvlist_free_binary(nvlist_t *nvl, void **cookiep);
-void cnvlist_free_bool_array(nvlist_t *nvl, void **cookiep);
-void cnvlist_free_number_array(nvlist_t *nvl, void **cookiep);
-void cnvlist_free_string_array(nvlist_t *nvl, void **cookiep);
-void cnvlist_free_nvlist_array(nvlist_t *nvl, void **cookiep);
-void cnvlist_free_binary_array(nvlist_t *nvl, void **cookiep);
-#ifndef _KERNEL
 void cnvlist_free_descriptor(nvlist_t *nvl, void **cookiep);
-void cnvlist_free_descriptor_array(nvlist_t *nvl, void **cookiep);
-#endif
 
 __END_DECLS
 
